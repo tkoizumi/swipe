38c0bea82986a00fc6ee8056ff6d336dcc8119ba
tkoizumi3 <tkoizumi3@gatech.edu>
2025-01-05 15:34:57 -0500

    add url argument

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Swipe program")
+	if len(os.Args) < 2 {
+		fmt.Println("Needs at least url as argument")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	fmt.Println("Sending request to " + url)
 }
