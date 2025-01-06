c2352c8412e5bc4c19308b177750b1f9e5796977
tkoizumi3 <tkoizumi3@gatech.edu>
2025-01-06 10:53:54 -0500

    add printing body

--- internal/core/response/response.go
+++ internal/core/response/response.go
@@ -2,6 +2,7 @@ package response
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -18,7 +19,18 @@ func Create(res *http.Response, err error) *response {
 func (r response) Print() {
 	if r.Err != nil {
 		fmt.Println("Response error")
+		fmt.Println(r.Err)
 		os.Exit(1)
 	}
-	fmt.Println(r.Res)
+
+	body, err := io.ReadAll(r.Res.Body)
+	if err != nil {
+		fmt.Println("Body response error")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(body))
+
+	r.Res.Body.Close()
 }
